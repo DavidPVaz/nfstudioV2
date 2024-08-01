e6fc4b97dda68a3a200d6f3a96a04d1faefbd560
David Vaz <davidpvaz@protonmail.com>
2024-08-01 12:43:46 +0100

    add resolution on how many data to show depending on window width

--- src/app/(home)/nfstudio-list.tsx
+++ src/app/(home)/nfstudio-list.tsx
@@ -1,18 +1,64 @@
 'use client';
 
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import { type CollectionConfiguration } from '@/server/service/mongo/types';
 import { CollectionsList } from '@/components/organisms';
+import { useWindowWidth } from '@/hooks';
+
+const BREAKPOINTS = {
+    XL: 1280,
+    LG: 1024,
+    MD: 768,
+    XS: 290
+};
+const COLLECTIONS_PER_BREAKPOINT = {
+    [BREAKPOINTS.XL]: 12,
+    [BREAKPOINTS.LG]: 10,
+    [BREAKPOINTS.MD]: 8,
+    [BREAKPOINTS.XS]: 6
+};
+
+type BreakpointCalculation = { width: number; previous: number | null };
+
+const isXLBreakpoint = ({ width, previous: previousBreakpoint }: BreakpointCalculation) =>
+    previousBreakpoint !== BREAKPOINTS.XL && width >= BREAKPOINTS.XL;
+const isLGBreakpoint = ({ width, previous: previousBreakpoint }: BreakpointCalculation) =>
+    previousBreakpoint !== BREAKPOINTS.LG && width >= BREAKPOINTS.LG && width < BREAKPOINTS.XL;
+const isMDBreakpoint = ({ width, previous: previousBreakpoint }: BreakpointCalculation) =>
+    previousBreakpoint !== BREAKPOINTS.MD && width >= BREAKPOINTS.MD && width < BREAKPOINTS.LG;
+const isXSBreakpoint = ({ width, previous: previousBreakpoint }: BreakpointCalculation) =>
+    previousBreakpoint !== BREAKPOINTS.XS && width >= BREAKPOINTS.XS && width < BREAKPOINTS.MD;
 
 export const NFStudioList = ({ collections }: { collections: CollectionConfiguration[] }) => {
-    // check window width with hook
-    // make calculations
+    const { width } = useWindowWidth();
+    const previous = useRef<number | null>(null);
+    const [numberOfCollectionsToShow, setNumberOfCollectionsToShow] = useState<number | null>(null);
 
-    // XL 12 - 1280px
-    // LG 10 - 1024px
-    // MD 8 - 768px
-    // XS: 6 - 290px
-    // 3
+    useLayoutEffect(() => {
+        if (isXLBreakpoint({ width: width ?? window.innerWidth, previous: previous.current })) {
+            previous.current = BREAKPOINTS.XL;
+            setNumberOfCollectionsToShow(COLLECTIONS_PER_BREAKPOINT[BREAKPOINTS.XL]);
+        } else if (
+            isLGBreakpoint({ width: width ?? window.innerWidth, previous: previous.current })
+        ) {
+            previous.current = BREAKPOINTS.LG;
+            setNumberOfCollectionsToShow(COLLECTIONS_PER_BREAKPOINT[BREAKPOINTS.LG]);
+        } else if (
+            isMDBreakpoint({ width: width ?? window.innerWidth, previous: previous.current })
+        ) {
+            previous.current = BREAKPOINTS.MD;
+            setNumberOfCollectionsToShow(COLLECTIONS_PER_BREAKPOINT[BREAKPOINTS.MD]);
+        } else if (
+            isXSBreakpoint({ width: width ?? window.innerWidth, previous: previous.current })
+        ) {
+            previous.current = BREAKPOINTS.XS;
+            setNumberOfCollectionsToShow(COLLECTIONS_PER_BREAKPOINT[BREAKPOINTS.XS]);
+        }
+    }, [width]);
 
-    return <CollectionsList collections={collections} />;
+    return (
+        numberOfCollectionsToShow && (
+            <CollectionsList collections={collections.slice(0, numberOfCollectionsToShow)} />
+        )
+    );
 };
