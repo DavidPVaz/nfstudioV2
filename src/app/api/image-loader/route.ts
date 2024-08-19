3496ad3819a10caf779134d69abaf65089ac0a90
David Vaz <davidpvaz@protonmail.com>
2024-08-19 22:10:05 +0100

    add data cache settings resolver in image loader optimizer

--- src/app/api/image-loader/route.ts
+++ src/app/api/image-loader/route.ts
@@ -4,22 +4,21 @@ import { optimize } from '@/server/service/image-loader';
 export async function GET(request: NextRequest) {
     // TODO: authorization + rate limit middleware + search params validation with zod
     const searchParams = Object.fromEntries(request.nextUrl.searchParams);
-    const { src, width, quality, maxAge = 31536000, sMaxAge = 31536000 } = searchParams;
+    const { src, width, quality, maxAge, sMaxAge } = searchParams;
 
     try {
-        const optimized = await optimize({ src, width, quality });
+        const optimized = await optimize({ src, width, quality, ttl: sMaxAge });
 
         return new NextResponse(optimized, {
             status: 200,
             headers: {
-                // TODO: verify this approach vs fetch revalidate option
-                'Cache-Control': `max-age=${maxAge}, s-maxage=${sMaxAge}, public, stale-while-revalidate=60`,
+                // TODO: verify this approach - check if works after deployed, otherwise need to move to pages router api
+                // Control the Edge Cache
+                'Cache-Control': `max-age=${maxAge ?? 31536000}, s-maxage=${sMaxAge ?? 31536000}, public, stale-while-revalidate=60`,
                 'Content-Type': 'image/webp'
             }
         });
     } catch (error) {
-        //await captureException(error);
-
         return new NextResponse('An error occurred while optimizing the image.', {
             status: 500
         });
