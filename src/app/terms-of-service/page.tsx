5a46ba4f49c42b940d268ee19df3aad4f7896fd0
David Vaz <davidpvaz@protonmail.com>
2024-08-20 14:28:32 +0100

    minor import change for bundle opt

--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Document } from '@/components/organisms';
+import { Document } from '@/components/organisms/document';
 import { queryDocument } from '@/server/service/contentful';
 import { DOCUMENTS } from '@/server/service/contentful/types';
 
