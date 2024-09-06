09f39feaece74ead018f7928b2affead633c54fc
David Vaz <davidpvaz@protonmail.com>
2024-09-06 18:37:29 +0100

    studio rsc

--- src/app/collections/[collection]/studio/index.tsx
+++ src/app/collections/[collection]/studio/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import type { CollectionConfiguration } from '@/server/service/mongo/types';
+import { Button } from '@/components/atoms/button';
+import { MarketplaceIcon, DiscordIcon, TwitterIcon, WebsiteIcon } from '@/resources';
 
-type CollectionLinks = {
+type StudioRSCProps = {
     collection: CollectionConfiguration['_id'];
     links: {
         marketplace: CollectionConfiguration['marketplace'];
@@ -11,16 +13,47 @@ type CollectionLinks = {
     };
 };
 
-export const Studio = ({ collection, links }: CollectionLinks) => {
-    return (
-        <div className="relative flex min-w-full flex-col items-center justify-start gap-y-12 md:gap-y-20">
-            <section className="relative flex min-w-full flex-col gap-3 sm:min-w-[80%] sm:flex-row md:min-w-[90%]">
-                <h2 className="text-start text-base leading-none text-foreground sm:text-xl">
-                    {collection}
-                </h2>
-                <div>{JSON.stringify(links)}</div>
-            </section>
-            <div className="relative flex min-w-full border sm:min-w-[80%] md:min-w-[90%]"></div>
+export const Studio = ({ collection, links }: StudioRSCProps) => (
+    <div className="relative flex min-w-full flex-col items-center justify-start gap-y-5 md:gap-y-8">
+        <CollectionIdentification collection={collection.replace('_', ' ')} links={links} />
+        <section className="relative flex max-h-[75vh] min-h-[75vh] w-full rounded-lg border sm:w-4/5">
+            {/* client content */}
+        </section>
+    </div>
+);
+
+const CollectionIdentification = ({ collection, links }: StudioRSCProps) => (
+    <section className="relative mt-2 flex w-full flex-col flex-wrap gap-1 xs:flex-row xs:items-center xs:gap-3 sm:w-4/5">
+        <h2 className="text-start text-2xl leading-none text-foreground sm:text-3xl">
+            {collection}
+        </h2>
+        <div className="relative flex flex-row">
+            <Button variant="ghost" size="icon" asChild>
+                <a href={links.marketplace} rel="noopener noreferrer" target={'_blank'}>
+                    <MarketplaceIcon className="h-[1.25rem] w-[1.25rem] fill-current sm:h-[1.4rem] sm:w-[1.4rem]" />
+                    <span className="sr-only">{`Buy ${collection} NFTs`}</span>
+                </a>
+            </Button>
+            <Button variant="ghost" size="icon" asChild>
+                <a href={links.discord} rel="noopener noreferrer" target={'_blank'}>
+                    <DiscordIcon className="h-[1.25rem] w-[1.25rem] fill-current sm:h-[1.4rem] sm:w-[1.4rem]" />
+                    <span className="sr-only">{`Go to ${collection} discord`}</span>
+                </a>
+            </Button>
+            <Button variant="ghost" size="icon" asChild>
+                <a href={links.twitter} rel="noopener noreferrer" target={'_blank'}>
+                    <TwitterIcon className="h-[.9rem] w-[.9rem] fill-current sm:h-[1rem] sm:w-[1rem]" />
+                    <span className="sr-only">{`Go to ${collection} twitter account`}</span>
+                </a>
+            </Button>
+            {links.website && (
+                <Button variant="ghost" size="icon" asChild>
+                    <a href={links.website} rel="noopener noreferrer" target={'_blank'}>
+                        <WebsiteIcon className="h-[1.25rem] w-[1.25rem] fill-current sm:h-[1.4rem] sm:w-[1.4rem]" />
+                        <span className="sr-only">{`Go to ${collection} website`}</span>
+                    </a>
+                </Button>
+            )}
         </div>
-    );
-};
+    </section>
+);
