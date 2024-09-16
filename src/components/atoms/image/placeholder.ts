fc954450da4e27c3360b2d3186e7a6f3b9c8b141
David Vaz <davidpvaz@protonmail.com>
2024-09-16 14:34:02 +0100

    changed image placeholder shimmer color

--- src/components/atoms/image/placeholder.ts
+++ src/components/atoms/image/placeholder.ts
@@ -4,12 +4,12 @@ const shimmer = () => `
 <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
+      <stop stop-color="#1e293b" offset="20%" />
+      <stop stop-color="#00142E33" offset="50%" />
+      <stop stop-color="#1e293b" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="100%" height="100%" fill="#333" />
+  <rect width="100%" height="100%" fill="#1e293b" />
   <rect id="r" width="100%" height="100%" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-100%" to="100%" dur="1s" repeatCount="indefinite"  />
 </svg>`;
@@ -19,5 +19,3 @@ const toBase64 = (string: string) =>
 
 export const generatePlaceholder = (): PlaceholderValue =>
     `data:image/svg+xml;base64,${toBase64(shimmer())}`;
-
-// TODO: replace this with the skeleton pulse animation?
