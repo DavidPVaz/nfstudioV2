49d7e007bd25062640284885d0c85f3f704f07f5
David Vaz <davidpvaz@protonmail.com>
2024-09-18 20:07:22 +0100

    add success color

--- src/app/collections/[collection]/studio/client/nfts-board/index.tsx
+++ src/app/collections/[collection]/studio/client/nfts-board/index.tsx
@@ -148,7 +148,7 @@ const Toolbar = React.memo(
                             notify({
                                 title: 'Whoops!',
                                 description: 'Message',
-                                duration: 6000,
+                                duration: 60000,
                                 variant: 'success'
                             });
                             //hasSelectedOneNft ? open wizard : show notification if user tries to create without having any selected nft
