2f84a24717eadebf88d8fc35dbd23aaa1aa8c3c6
David Vaz <davidpvaz@protonmail.com>
2024-10-02 18:18:22 +0100

    add config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
@@ -6,12 +7,91 @@ import { withSentryConfig } from '@sentry/nextjs';
 import Analyzer from '@next/bundle-analyzer';
 const withBundleAnalyzer = Analyzer({ enabled: process.env.ANALYZE === 'true' });
 
+let ContentSecurityPolicy = `
+    navigate-to 'self';
+    style-src 'self' 'unsafe-inline' https://embed.hel.io/assets/index-v1.css;
+    img-src 'self' data: https:;
+    worker-src 'self';
+    connect-src 'self' wss://relay.walletconnect.com wss://www.walletlink.org/rpc https:;
+    font-src 'self' https://helio-assets.s3.eu-west-1.amazonaws.com/fonts/inter-var-latin.woff2 https://helio-assets.s3.eu-west-1.amazonaws.com/fonts/Poppins-Bold.woff2 https://helio-assets.s3.eu-west-1.amazonaws.com/fonts/Poppins-Regular.woff2;
+    object-src 'none';
+    media-src 'none';
+    frame-src https://verify.walletconnect.com/ https://verify.walletconnect.org/;
+    manifest-src 'self';
+    base-uri 'none';
+    form-action 'none';`;
+
+// adds extra conditions both in production and preview(staging) environments
+if (process.env.VERCEL_ENV !== 'development') {
+    ContentSecurityPolicy = ContentSecurityPolicy.concat(
+        " script-src https://embed.hel.io/assets/ 'self' 'sha256-q1+DaXsZUnEJs3jpN9ZoWp6ypK1xBwXiRxG+C31xOUA='; default-src 'none';"
+    );
+}
+
+const headers = [
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+    },
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+    },
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+    }
+];
+
+const robotTagHeader = {
+    key: 'X-Robots-Tag',
+    value: 'noindex'
+};
+
+const sources = ['/api/:path*', '/_next/data/:path*', '/_next/image/:path*'];
+
+// adds header both in development and preview(staging) environments
+if (process.env.VERCEL_ENV !== 'production') {
+    headers.push(robotTagHeader);
+}
+
 const config = {
     reactStrictMode: false,
+    productionBrowserSourceMaps: false,
     experimental: {
         instrumentationHook: true
     },
-    webpack: config => {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers
+            },
+            // no matter the environment, all these sources will have X-Robot-Tag = noindex
+            ...sources.map(source => ({ source, headers: [robotTagHeader] })),
+            {
+                // matching all API routes
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: process.env.ORIGIN },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,POST'
+                    }
+                ]
+            }
+        ];
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**'
+            }
+        ]
+    },
+    webpack: (config, { webpack }) => {
         config.module.rules.push(
             {
                 test: /\.svg$/,
@@ -28,6 +108,14 @@ const config = {
                 ],
                 sideEffects: false
             }
+        );
+
+        //https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/tree-shaking/
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                __SENTRY_DEBUG__: false,
+                __SENTRY_TRACING__: false
+            })
         );
 
         return config;
