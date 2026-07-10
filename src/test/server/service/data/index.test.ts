72a4f9da715d7c3c6663fcee1f6e571420cabb16
David Vaz <davidpvaz@protonmail.com>
2026-07-10 12:17:59 +0100

    fix(test): remove unused.

--- src/test/server/service/data/index.test.ts
+++ src/test/server/service/data/index.test.ts
@@ -1,15 +1,4 @@
 import { describe, vi, afterEach } from 'vitest';
-import {
-    queryCollectionsData,
-    queryMetadata,
-    insertRefundTransaction,
-    queryVerifiedRefundTransactionsToProcess,
-    updateManyRefundTransactions,
-    queryUnverifiedRefundTransactionsToReevaluate,
-    updateOneRefundTransaction,
-    deleteInvalidRefundTransactions,
-    batch
-} from '@/server/service/data';
 
 const { getDbConnectionMock, buildQueryMock } = vi.hoisted(() => ({
     getDbConnectionMock: vi.fn(),
